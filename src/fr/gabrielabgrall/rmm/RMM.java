92e77a6b3826093f59da3d9b064d8428c7ecd575
GabrielAbgrall <gabriel.abgrall@gmail.com>
2024-10-22 14:56:33 +0200

    DB connection

--- src/fr/gabrielabgrall/rmm/RMM.java
+++ src/fr/gabrielabgrall/rmm/RMM.java
@@ -1,10 +1,15 @@
 package fr.gabrielabgrall.rmm;
 
+import fr.gabrielabgrall.rmm.database.DatabaseConnector;
 import fr.gabrielabgrall.rmm.utils.Debug;
 
 public class RMM {
 
     public static void main(String[] args) {
         Debug.setDebug(true);
+
+        DatabaseConnector dbConnector = new DatabaseConnector("local.db");
+        dbConnector.connect();
+        dbConnector.close();
     }
 }
--- src/fr/gabrielabgrall/rmm/database/DatabaseConnector.java
+++ src/fr/gabrielabgrall/rmm/database/DatabaseConnector.java
@@ -1,1 +1,39 @@
 package fr.gabrielabgrall.rmm.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import fr.gabrielabgrall.rmm.utils.Debug;
+
+public class DatabaseConnector {
+
+    protected Connection conn;
+    protected String url;
+
+    public DatabaseConnector(String url) {
+        this.url = "jdbc:sqlite:" + url;
+    }
+
+    public void connect() {
+        try {
+            conn = DriverManager.getConnection(url);
+            Debug.log("Connection to database ", url, " successful");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            conn.close();
+            Debug.log("Connection to database ", url, " closed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() {
+        //todo
+    }
+}
