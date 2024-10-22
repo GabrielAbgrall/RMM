f5bebe018cdb981d7c6b7379dd52aa25630d12ec
GabrielAbgrall <gabriel.abgrall@gmail.com>
2024-10-22 14:56:36 +0200

    DB Connection

--- src/fr/gabrielabgrall/rmm/database/DatabaseConnector.java
+++ src/fr/gabrielabgrall/rmm/database/DatabaseConnector.java
@@ -3,6 +3,7 @@ package fr.gabrielabgrall.rmm.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import fr.gabrielabgrall.rmm.utils.Debug;
 
@@ -19,6 +20,8 @@ public class DatabaseConnector {
         try {
             conn = DriverManager.getConnection(url);
             Debug.log("Connection to database ", url, " successful");
+            init();
+            Debug.log("Database ", url, " successfuly initialized");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -33,7 +36,25 @@ public class DatabaseConnector {
         }
     }
 
-    public void init() {
-        //todo
+    public void init() throws SQLException {
+        Statement stmt = conn.createStatement();
+        /*stmt.execute(
+                "CREATE TABLE IF NOT EXISTS test ("
+            +   "   id      INTEGER PRIMARY KEY,"
+            +   "   name    TEXT NOT NULL"
+            +   ")"
+        );*/
+
+        /*
+        TABLES :
+        client (id_client, login, password)
+        script (id_script, content)
+        script_routine (id_script, id_client, periodicity)
+        client_data (cpu, ram, memory)
+        */
+    }
+
+    public Connection getConn() {
+        return conn;
     }
 }
